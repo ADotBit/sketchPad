945056bf5a87df955864f698126af624336441c4
Axel <eldrank@gmail.com>
2022-08-25 09:05:22 +0200

    Add resetBoard and make clear Button

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".reset");
+const clearBtn = document.querySelector(".clear");
+const div = Array.from(document.querySelectorAll("div"));
+
 const defaultSize = 16;
 
 function makeBoard(size){
@@ -13,15 +17,24 @@ function makeBoard(size){
 
 }
 
-function clearBoard(){
+function resetBoard(){
     container.innerHTML = "";
 };
+
+function clearBoard(){
+    const box = document.querySelectorAll(".box");
+    box.forEach((item) => {
+        item.style.backgroundColor = "transparent";
+    })
+}
+clearBtn.addEventListener("click", clearBoard);
+ 
 
 function changeBoard(){
     const userButton = document.querySelector(".getUser");
     userButton.addEventListener("click", () => {
         let getSize = parseInt(prompt("Choose the size of the board: "));
-        clearBoard();
+        resetBoard();
         makeBoard(getSize);
     })
 }
@@ -34,7 +47,6 @@ function makeColor(){
 }
 
 //Target each box of the grid and change color
-const div = Array.from(document.querySelectorAll("div"));
 div.forEach((box) => {
     box.addEventListener("mousedown", makeColor); //mousedown is the click
     box.addEventListener("mouseover", event => { //mouseover is to continue the action when stay click
@@ -43,10 +55,9 @@ div.forEach((box) => {
 });   
 
 //Reset everything by reloading the page
-const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", () => location.reload());
 
 window.onload = () => {
-    makeBoard(defaultSize)
-    changeBoard()
+    makeBoard(defaultSize);
+    changeBoard();
 }
